52d54bc0577689a1c08943e5188951aeb9ad6723
Panitnun Suvannabun <panitnun.6243@gmail.com>
2023-08-26 04:11:46 +0700

    Add prerequisite

---
+++ first-step.sh
@@ -0,0 +1,7 @@
+# install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# install git
+brew install git
+
+# clone this repo after installing git
