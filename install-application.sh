392b0227078c3b8a541485dfcee37f1c71c3dee5
Panitnun Suvannabun <panitnun.6243@gmail.com>
2023-08-26 05:31:25 +0700

    Add install-app-scripts

---
+++ install-application.sh
@@ -0,0 +1,52 @@
+# run .sh file by typing: sh and drag this file to terminal
+# CLI apps
+brew install tree
+brew install wget
+brew install cmatrix
+brew install docker
+brew install nvm
+brew install go
+
+
+# GUI apps
+brew install --cask github
+brew install --cask visual-studio-code
+brew install --cask jetbrains-toolbox
+brew install --cask termius
+brew install --cask microsoft-teams
+brew install --cask telegram
+brew install --cask discord
+brew install --cask rectangle
+brew install --cask google-chrome
+brew install --cask slack
+brew install --cask dbeaver-community
+brew install --cask anaconda
+brew install --cask postman
+brew install --cask android-studio
+brew install --cask bilibili
+brew install --cask keyboard-cleaner
+brew install --cask messenger
+brew install --cask zoom
+brew install --cask cheatsheet
+brew install --cask bitwarden
+brew install --cask appcleaner
+brew install --cask dropzone
+brew install --cask itsycal
+brew install --cask scroll-reverser
+# brew install --cask command-x
+# brew install --cask google-cloud-sdk
+# brew install --cask topnotch
+# brew install --cask steam
+
+
+# App Store apps
+# Amphetamine
+# CopyClip
+# CapCut
+# Handmirror
+
+
+# Crack apps
+# istat menus
+# bartender
+# alfred power pack
